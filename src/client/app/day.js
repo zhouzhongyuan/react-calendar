acfb9708c38be2f742bfaf6b92e5372f2b1241d4
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2016-10-03 21:54:36 +0800

    add DayView

---
+++ src/client/app/day.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class DayView extends React.Component {
+    render() {
+        var hourNumberList = [];
+        for (var i=0; i < 24; i++) {
+            hourNumberList.push(<div key={i} style={{height:42}}><div className="tg-time-pri" style={{height:42}}>{i}:00</div></div>);
+        }
+        var hourMarkerList = [];
+        for (var i=0; i < 24; i++) {
+            hourMarkerList.push(<div key={i} className="tg-markercell"><div className="tg-dualmarker"></div></div>);
+        }
+        return (
+            <section className="time-event" style={{
+                width:'100%',
+                flex:1,
+                overflow: 'scroll'
+            }}>
+                <section className="hour-number">{hourNumberList}</section>
+                <section className="hour-marker">{hourMarkerList}</section>
+            </section>
+        );
+    }
+}
--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import {render} from 'react-dom';
+import DayView from './day';
 
 class App extends React.Component {
-    render () {
-        return <p> Hello React!</p>;
+    render() {
+        return (
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                overflow: 'hidden',
+                width: '100%',
+                height: '100%'
+            }}>
+                <p style={{height:60}}>Calendar</p>
+                <DayView/>
+            </div>
+        );
     }
 }
 
